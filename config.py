9677e9e6a9b230a4d05ed094c9f436155bb981ec
Novaxking <totogenius.a.n.k@gmail.com>
2025-03-02 17:00:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ PORT = os.environ.get("PORT", "8030")
 
 #Database
 DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "KZENDB")
+DB_NAME = os.environ.get("DATABASE_NAME", "bot13")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
