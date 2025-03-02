767cb59f00874456cd39709b586034ed7f6981d7
Novaxking <totogenius.a.n.k@gmail.com>
2025-03-02 17:02:59 +0300

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -16,7 +16,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 [
                     [
                     InlineKeyboardButton("⚡️ ᴄʟᴏsᴇ", callback_data = "close"),
-                    InlineKeyboardButton('🍁 ADs & B/s', url= 't.me/roverxstar')
+                    InlineKeyboardButton('🍁 ADs & B/s', url= 'https://t.me/roverxstar')
                     ]
                 ]
             )
